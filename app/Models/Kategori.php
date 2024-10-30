7e86a751704c5c2628d915461d122268c90b560f
evinovita13 <evi770994@gmail.com>
2024-10-30 22:25:35 +0700

    migration categories

--- app/Models/Kategori.php
+++ app/Models/Kategori.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Kategori extends Model
 {
-    /** @use HasFactory<\Database\Factories\KategoriFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'kategoris';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+    ];
 }
--- database/migrations/2024_10_30_145958_create_kategoris_table.php
+++ database/migrations/2024_10_30_145958_create_kategoris_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('kategoris', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name',255)->nullable();
             $table->string('description',255)->nullable();
